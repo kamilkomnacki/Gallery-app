b33cdfb0708cead317d47d2092a155b933bf241f
kamil.komnacki <kamilkomnacki@gmail.com>
2021-02-21 23:55:47 +0100

    Format GalleryFragment

--- app/src/main/java/komnacki/gallery/login/Constants.kt
+++ app/src/main/java/komnacki/gallery/login/Constants.kt
@@ -6,6 +6,7 @@ object Constants {
     const val BASE_API_URL = "https://api.unsplash.com/"
     const val API_TOKEN = "CGMrIUppQWgHAOi6sy-beLfhcCyNm1NKUW7SnX_vspc"
 
+    const val GALLERY_DEFAULT_COLUMN_WIDTH = 300
     const val PASSWORD_MIN_LENGTH = 5
     const val LOGIN_REQUEST_TIMEOUT_SECONDS: Long = 10
     const val GALLERY_TAG = "Gallery"
--- app/src/main/java/komnacki/gallery/login/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/komnacki/gallery/login/ui/gallery/GalleryFragment.kt
@@ -3,7 +3,6 @@ package komnacki.gallery.login.ui.gallery
 import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -12,30 +11,29 @@ import androidx.paging.LoadState
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import komnacki.gallery.R
+import komnacki.gallery.login.Constants
 import kotlinx.android.synthetic.main.fragment_gallery.*
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
-    private companion object Constants {
+    private companion object {
         const val IS_DIALOG_VISIBLE = "IS_DIALOG_VISIBLE"
     }
 
     private val viewModel by activityViewModels<GalleryViewModel>()
     private val adapter: GalleryAdapter = GalleryAdapter {
         viewModel.select(it)
-//        if(viewModel.isUserLogIn()) {
+        if(viewModel.isUserLogIn()) {
             this.findNavController().navigate(R.id.toDetailFragment)
-//        } else {
-//            dialog.show()
-//        }
+        } else {
+            dialog.show()
+        }
     }
     private lateinit var dialog: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("KK: ", "Gallery fragment onCreate")
-
         dialog = getLoginDialog(requireContext())
 
         val isDialogVisible = savedInstanceState?.getBoolean(IS_DIALOG_VISIBLE)
@@ -46,7 +44,6 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d("KK: ", "Gallery fragment onViewCreated")
 
         setUpStateListener { showSnackbarError(view) }
         setUpViews()
@@ -87,26 +84,24 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     }
 
     private fun setUpViews() {
-        rv_gallery.layoutManager = GridAutofillLayoutManager(requireContext(), 300)
+        rv_gallery.layoutManager = GridAutofillLayoutManager(requireContext(), Constants.GALLERY_DEFAULT_COLUMN_WIDTH)
         rv_gallery.adapter = adapter
     }
 
     private fun showSnackbarError(parent: View) {
-        Snackbar.make(parent, "Something went wrong, please try again", Snackbar.LENGTH_INDEFINITE)
-            .setAction("Retry") {
+        Snackbar.make(parent, getString(R.string.gallery_snackbar_message), Snackbar.LENGTH_INDEFINITE)
+            .setAction(getString(R.string.gallery_retry_button)) {
                 adapter.retry()
             }.show()
     }
 
     private fun getLoginDialog(context: Context): AlertDialog {
         return AlertDialog.Builder(context)
-            .setTitle("Account Required")
-            .setPositiveButton("Sing in") { dialogInterface, i ->
-                Log.d("KK: ", "login")
+            .setTitle(getString(R.string.gallery_dialog_title))
+            .setPositiveButton(getString(R.string.gallery_dialog_positive_button)) { dialogInterface, i ->
                 this.findNavController().navigate(R.id.toLoginFragment)
             }
-            .setNegativeButton("Cancel") { dialogInterface, i ->
-                Log.d("KK: ", "login cancel")
+            .setNegativeButton(getString(R.string.gallery_dialog_negative_button)) { dialogInterface, i ->
                 dialogInterface.dismiss()
             }.create()
     }
