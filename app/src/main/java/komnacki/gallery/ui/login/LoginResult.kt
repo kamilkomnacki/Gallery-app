5ca1794c47726a7c3ae2cfafc9e66ff8374f98c7
kamil.komnacki <kamilkomnacki@gmail.com>
2021-02-22 00:24:02 +0100

    Remove comment

--- app/src/main/java/komnacki/gallery/ui/login/LoginResult.kt
+++ app/src/main/java/komnacki/gallery/ui/login/LoginResult.kt
@@ -1,8 +1,5 @@
 package komnacki.gallery.ui.login
 
-/**
- * Authentication result : success (user details) or error message.
- */
 data class LoginResult(
         val success: Int? = null,
         val error: Int? = null,
