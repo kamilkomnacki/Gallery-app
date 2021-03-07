7a1eb95232acf7b6f36b5fd327a4be443c2635f7
kamil.komnacki <kamilkomnacki@gmail.com>
2021-03-07 21:51:57 +0100

    Add parcelable to AppUser

--- app/src/main/java/komnacki/gallery/data/model/AppUser.kt
+++ app/src/main/java/komnacki/gallery/data/model/AppUser.kt
@@ -1,8 +1,11 @@
 package komnacki.gallery.data.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class AppUser(
     @SerializedName("username")
     val username: String,
-)
+) : Parcelable
